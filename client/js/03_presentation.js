a3d4cd4b5d95b5d28087486021a3d9dd08937cfa
Chema Rivera <chema@iMac-de-Chema.local>
2018-06-23 19:13:21 +0200

    great update into the presentation area

--- client/js/03_presentation.js
+++ client/js/03_presentation.js
@@ -2,13 +2,400 @@ $(function() {
     var blockSlider = false;
     var sliderSpeedMult = 2;
     var sliderSpeed, slideHeight, slideLeft;
+    var curriculum = [
+        {
+            title: 'spanish',
+            url: 'https://drive.google.com/file/d/1EIjlQ1Zv4wf7v92kyCPZz0wDqiCzqBJ8/view?usp=sharing',
+            personal: {
+                name: 'José María Rivera Alonso',
+                role: 'Desarrollador FullStack',
+                email: 'chema22r@gmail.com',
+                phone: '(+34) 697946985',
+                country: {
+                    img: 'img/spain.png',
+                    name: 'España'
+                },
+                birthday: '28/09/1995'
+            },
+            training: {
+                title: 'Formación',
+                list: [
+                    {
+                        title: 'Máster en Ingeniería Informática',
+                        period: '(09/2017 - presente)',
+                        level: 'EQF-MEC 7',
+                        location: 'Universidad de León, León (España)'
+                    },
+                    {
+                        title: 'Grado en Ingeniería Informática',
+                        period: '(09/2013 - 06/2017)',
+                        level: 'EQF-MEC 6',
+                        location: 'Universidad de León, León (España)'
+                    }
+                ]
+            },
+            courses: {
+                title: 'Cursos',
+                list: [
+                    {
+                        title: 'Emprendimiento Social',
+                        period: '(2017)',
+                        duration: '30 horas',
+                        location: 'Miríada X'
+                    },
+                    {
+                        title: 'Taller Emprendedor ágil. Lean Startup en el canal online',
+                        period: '(2017)',
+                        duration: '20 horas',
+                        location: 'Fundación General de la Universidad de León y de la empresa, FGULEM'
+                    },
+                    {
+                        title: 'Curso básico de Marketing Digital',
+                        period: '(2016)',
+                        duration: '40 horas',
+                        location: 'Programa Actívate, Google'
+                    },
+                    {
+                        title: 'Diseño web con HTML5 y CSS3',
+                        period: '(2015)',
+                        duration: '120 horas',
+                        location: 'Seis Cocos Technical School'
+                    },
+                    {
+                        title: 'Unity 3D',
+                        period: '(2015)',
+                        duration: '40 horas',
+                        location: 'Universidad de León'
+                    },
+                    {
+                        title: 'Desarrollo web con Joomla',
+                        period: '(2015)',
+                        duration: '',
+                        location: 'Cowabi Web Solutions'
+                    },
+                    {
+                        title: 'Programación en C',
+                        period: '(2014)',
+                        duration: '40 horas',
+                        location: 'Universidad de León'
+                    }
+                ]
+            },
+            workExp: {
+                title: 'Experiencia Profesional',
+                list: [
+                    {
+                        title: 'Modelador 3D',
+                        location: 'Universidad de León, León (España)',
+                        period: '09/2015–presente',
+                        description: 'Proyecto de investigación para el desarrollo de prótesis de bajo coste para animales con miembros amputados, mediante el uso de escaneado, modelado e impresión 3D. Encargado de todas las tareas descritas anteriormente.'
+                    },
+                    {
+                        title: 'Profesor de clases particulares',
+                        location: '',
+                        period: '09/2015–presente',
+                        description: 'Profesor de clases particulares para alumnos de Ingenierías y enseñanzas técnicas.<br>Materias impartidas: Java y desarrollo web.'
+                    },
+                    {
+                        title: 'Desarrollador Web FullStack',
+                        location: 'HP SCDS, León (España)',
+                        period: '11/2016–07/2017',
+                        description: 'Creación de un previsualizador de impresión 3D utilizando tecnologías HTML5/JS. Este proyecto constituye las prácticas de empresa y el proyecto final del Grado en Ingeniería Informática. Además, fue premiado como el mejor proyecto del XII Observatorio Tecnológico de HP.'
+                    },
+                    {
+                        title: 'Profesor invitado',
+                        location: 'Universidad de León, León (España)',
+                        period: '09/2016',
+                        description: 'Profesor en la Universidad de León durante el curso de extensión universitaria Unity 3D. Encargado de parte de las clases relacionadas con Autodesk Maya y modelado 3D.'
+                    }
+                ]
+            },
+            langs: {
+                title: 'Idiomas',
+                motherTongue: {
+                    title: 'Lengua materna',
+                    val: 'Español'
+                },
+                other: {
+                    title: 'Otros idiomas',
+                    val: 'Inglés (nivel B2)<br>Japonés (nivel A2/A1)'
+                }
+            },
+            awards: {
+                title: 'Premios y Distinciones',
+                list: [
+                    {
+                        title: 'Mejor proyecto del XII Observatorio Tecnológico de HP',
+                        period: '2017',
+                        description: 'Ganador del XII Observatorio Tecnológico de HP, por el desarrollo de un previsualizador de impresión 3D utilizando tecnologías HTML5/JS.​'
+                    }
+                ]
+            }
+        },
+        {
+            title: 'english',
+            url: 'https://drive.google.com/file/d/1UsV83YE_Dq2WZ3Sfj_qCT5_LwcMgWYEs/view?usp=sharing',
+            personal: {
+                name: 'José María Rivera Alonso',
+                role: 'FullStack Developer',
+                email: 'chema22r@gmail.com',
+                phone: '(+34) 697946985',
+                country: {
+                    img: 'img/spain.png',
+                    name: 'Spain'
+                },
+                birthday: '28/09/1995'
+            },
+            training: {
+                title: 'Training',
+                list: [
+                    {
+                        title: 'Master’s Degree in Computer Engineering',
+                        period: '(09/2017 - present)',
+                        level: 'EQF 7',
+                        location: 'University of León, León (Spain)'
+                    },
+                    {
+                        title: 'Bachelor’s Degree in Computer Engineering',
+                        period: '(09/2013 - 06/2017)',
+                        level: 'EQF 6',
+                        location: 'University of León, León (Spain)'
+                    }
+                ]
+            },
+            courses: {
+                title: 'Courses',
+                list: [
+                    {
+                        title: 'Social Entrepreneurship',
+                        period: '(2017)',
+                        duration: '30 hours',
+                        location: 'Miríada X'
+                    },
+                    {
+                        title: 'Agile Entrepreneur Workshop. Lean Startup on the online channel',
+                        period: '(2017)',
+                        duration: '20 hours',
+                        location: 'General Foundation of the University of León and the company, FGULEM'
+                    },
+                    {
+                        title: 'Basic Digital Marketing Course',
+                        period: '(2016)',
+                        duration: '40 hours',
+                        location: 'Actívate program, Google'
+                    },
+                    {
+                        title: 'Web design with HTML5 and CSS3',
+                        period: '(2015)',
+                        duration: '120 hours',
+                        location: 'Seis Cocos Technical School'
+                    },
+                    {
+                        title: 'Unity 3D',
+                        period: '(2015)',
+                        duration: '40 hours',
+                        location: 'University of León'
+                    },
+                    {
+                        title: 'Web development with Joomla',
+                        period: '(2015)',
+                        duration: '',
+                        location: 'Cowabi Web Solutions'
+                    },
+                    {
+                        title: 'C programming',
+                        period: '(2014)',
+                        duration: '40 hours',
+                        location: 'University of León'
+                    }
+                ]
+            },
+            workExp: {
+                title: 'Work Experience',
+                list: [
+                    {
+                        title: '3D Modeler',
+                        location: 'University of León, León (Spain)',
+                        period: '09/2015–present',
+                        description: 'Research project to develop low-cost prostheses for animals with amputated limbs, through the use of scanning, modeling and 3D printing. In charge of all the tasks described above.'
+                    },
+                    {
+                        title: 'Private tutor',
+                        location: '',
+                        period: '09/2015–present',
+                        description: 'Private tutor for students of Engineering and technical education.<br>Subjects taught: Java and web development.'
+                    },
+                    {
+                        title: 'FullStack Web Developer',
+                        location: 'HP SCDS, León (Spain)',
+                        period: '11/2016–07/2017',
+                        description: "Creation of a 3D print previewer using HTML5/JS technologies. This project constitutes the company practices and the final project of the Bachelor's Degree in Computer Engineering. In addition, it was awarded as the best project of the XII Technology Observatory of HP."
+                    },
+                    {
+                        title: 'Guest professor',
+                        location: 'University of León, León (Spain)',
+                        period: '09/2016',
+                        description: 'Professor at the University of León during the university extension course Unity 3D. In charge of the classes related to Autodesk Maya and 3D modeling.'
+                    }
+                ]
+            },
+            langs: {
+                title: 'Languages',
+                motherTongue: {
+                    title: 'Mother tongue',
+                    val: 'Spanish'
+                },
+                other: {
+                    title: 'Other languages',
+                    val: 'English (level B2)<br>Japanese (level A2/A1)'
+                }
+            },
+            awards: {
+                title: 'Honours and Awards',
+                list: [
+                    {
+                        title: 'Best project of the XII Technology Observatory of HP',
+                        period: '2017',
+                        description: '​Winner of the XII Technology Observatory of HP, for the development of a 3D print previewer using HTML5/JS technologies.​'
+                    }
+                ]
+            }
+        }
+    ];
+    var social = [
+        {
+            title: 'linkedin',
+            url: 'https://www.linkedin.com/in/chema22r'
+        },
+        {
+            title: 'facebook',
+            url: 'https://www.facebook.com/Chema22R'
+        },
+        {
+            title: 'gmail',
+            url: 'mailto:' + curriculum[0].personal.email
+        }
+    ];
 
+    generateSlides();
     generateSlideCounter();
+
+    function generateSlides() {
+        var codeHTML, slides;
+
+        codeHTML =  '<div class="slide training courses"></div>' +
+                    '<div class="slide workExp"></div>' +
+                    '<div class="slide langs awards"></div>';
+
+        $('#slidesContainer').append(codeHTML);
+
+
+        slides = $('#slidesContainer > .slide');
+        $(slides[0]).addClass('current');
+        $(slides[1]).addClass('next');
+        $(slides[slides.length-1]).addClass('previous');
+
+
+        for (var i=0; i<social.length; i++) {
+            codeHTML =  '<a class="socialLink icon-' + social[i].title + ' icon" href="' + social[i].url + '" target="_blank" rel="external"></a>';
+            $('#presentation > .section.social').append(codeHTML);
+        }
+
+
+        for (var i=0; i<curriculum.length; i++) {
+            codeHTML =  '<a class="socialLink ' + curriculum[i].title + 'Content icon-cv icon" href="' + curriculum[i].url + '" target="_blank" rel="external"></a>';
+            $('#presentation > .section.social').append(codeHTML);
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].personal.name + '</h2>' +
+                            '<h3>' + curriculum[i].personal.role + '</h3>' +
+                            '<p>' + curriculum[i].personal.email + ' | ' + curriculum[i].personal.phone + '</p>' +
+                            '<p class="country">' +
+                                '<img src="' + curriculum[i].personal.country.img + '" alt="' + curriculum[i].personal.country.name + '">' +
+                                curriculum[i].personal.country.name + ' | ' + curriculum[i].personal.birthday +
+                            '</p>' +
+                        '</div>';
+
+            $('#presentation > .section.basic').append(codeHTML);   // end basic or personal info
+
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].training.title + '</h2>';
+
+            for (var j=0; j<curriculum[i].training.list.length; j++) {
+                codeHTML += '<h3 class="bold nbsp">' + curriculum[i].training.list[j].title + '</h3>' +
+                            '<h3 class="size01 nbsp lineBreak">' + curriculum[i].training.list[j].period + '</h3>' +
+                            '<h3 class="size02 floatR lineBreak">' + curriculum[i].training.list[j].level + '</h3>' +
+                            '<p>' + curriculum[i].training.list[j].location + '</p>';
+            }
+
+            codeHTML += '<br></div>';
+
+            $('#slidesContainer > .slide.training').append(codeHTML);   // end training info
+
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].courses.title + '</h2>';
+
+            for (var j=0; j<curriculum[i].courses.list.length; j++) {
+                codeHTML += '<h3 class="size01 bold nbsp">' + curriculum[i].courses.list[j].title + '</h3>' +
+                            '<h3 class="size02 nbsp lineBreak">' + curriculum[i].courses.list[j].period + '</h3>' +
+                            '<h3 class="size02 floatR lineBreak">' + curriculum[i].courses.list[j].duration + '</h3>' +
+                            '<p>' + curriculum[i].courses.list[j].location + '</p>';
+            }
+
+            codeHTML += '</div>';
+
+            $('#slidesContainer > .slide.courses').append(codeHTML);    // end courses info
+
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].workExp.title + '</h2>';
+
+            for (var j=0; j<curriculum[i].workExp.list.length; j++) {
+                codeHTML += '<h3 class="bold nbsp">' + curriculum[i].workExp.list[j].title + '</h3>' +
+                            '<h3 class="size01 nbsp lineBreak">' + curriculum[i].workExp.list[j].location + '</h3>' +
+                            '<h3 class="size01 floatR lineBreak">' + curriculum[i].workExp.list[j].period + '</h3>' +
+                            '<p>' + curriculum[i].workExp.list[j].description + '</p>';
+            }
+
+            codeHTML += '</div>';
+
+            $('#slidesContainer > .slide.workExp').append(codeHTML);    // end work experience info
+
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].langs.title + '</h2>' +
+                            '<h3 class="size01 bold nbsp">' + curriculum[i].langs.motherTongue.title + '</h3>' +
+                            '<h3 class="size01">' + curriculum[i].langs.motherTongue.val + '</h3>' +
+                            '<br>' +
+                            '<h3 class="size01 bold nbsp alignTop">' + curriculum[i].langs.other.title + '</h3>' +
+                            '<h3 class="size01">' + curriculum[i].langs.other.val + '</h3>' +
+                            '<br><br>' +
+                        '</div>';
+
+            $('#slidesContainer > .slide.langs').append(codeHTML);    // end languages info
+
+
+            codeHTML =  '<div class="' + curriculum[i].title + 'Content">' +
+                            '<h2>' + curriculum[i].awards.title + '</h2>';
+
+            for (var j=0; j<curriculum[i].awards.list.length; j++) {
+                codeHTML += '<h3 class="size01 bold nbsp">' + curriculum[i].awards.list[j].title + '</h3>' +
+                            '<h3 class="size01 floatR lineBreak">' + curriculum[i].awards.list[j].period + '</h3>' +
+                            '<p>' + curriculum[i].awards.list[j].description + '</p>';
+            }
+
+            codeHTML += '</div>';
+
+            $('#slidesContainer > .slide.awards').append(codeHTML);    // end awards info
+        }
+    }
 
     function generateSlideCounter() {
         var code = "";
 
-        for (var i=0; i<$(".section.slides .slide").length; i++) {
+        for (var i=0; i<$(".section.slides > div > .slide").length; i++) {
             code += "<button></button>";
         }
 
