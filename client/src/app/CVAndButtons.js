9757cd9ab016efef3cdffa7d3cf93cea90e9a40e
Jose Maria Rivera <chema22r@gmail.com>
2019-09-15 00:42:38 +0200

    Slides removed in mobile devices and some style modifications (#28)

--- client/src/app/CVAndButtons.js
+++ client/src/app/CVAndButtons.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const isMobile = {  // allows to detect mobile devices
+    Android: () => {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: () => {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: () => {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: () => {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: () => {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: () => {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 const sliderSpeedMult = 2;
 var blockSlider = false;
 var sliderSpeed, slideHeight, slideLeft;
@@ -12,7 +32,7 @@ fetch(DATA_URL)
 function generateSlides(curriculum, langs) {
     let codeHTML, slides;
 
-    codeHTML =  '<div class="slide education courses"></div><div class="slide workExp"></div><div class="slide langs awards"></div>';
+    codeHTML =  '<div class="slide workExp"></div><div class="slide education courses"></div><div class="slide langs awards"></div>';
     $('#slidesContainer').append(codeHTML);
 
 
@@ -44,13 +64,29 @@ function generateSlides(curriculum, langs) {
 
 
         codeHTML =  '<div class="' + langs[i] + ' content">' +
+                        '<h2>' + curriculum.workExp.title[langs[i]] + '</h2>';
+
+        for (let j=0; j<curriculum.workExp.list.length; j++) {
+            codeHTML += '<h3 class="bold nbsp">' + curriculum.workExp.list[j].title[langs[i]] + '</h3>' +
+                        '<h3 class="size01 nbsp lineBreak">' + curriculum.workExp.list[j].location[langs[i]] + '</h3>' +
+                        '<h3 class="size01 floatR lineBreak">' + curriculum.workExp.list[j].period[langs[i]] + '</h3>' +
+                        '<p class="padding-left">' + curriculum.workExp.list[j].description[langs[i]] + '</p>';
+        }
+
+        codeHTML += '</div>';
+
+        $('#slidesContainer > .slide.workExp').append(codeHTML);    // end work experience info
+
+
+        codeHTML =  '<div class="' + langs[i] + ' content">' +
                         '<h2>' + curriculum.education.title[langs[i]] + '</h2>';
 
         for (let j=0; j<curriculum.education.list.length; j++) {
             codeHTML += '<h3 class="bold nbsp">' + curriculum.education.list[j].title[langs[i]] + '</h3>' +
                         '<h3 class="size01 nbsp lineBreak">' + curriculum.education.list[j].period + '</h3>' +
                         '<h3 class="size02 floatR lineBreak">' + curriculum.education.list[j].level[langs[i]] + '</h3>' +
-                        '<p>' + curriculum.education.list[j].location[langs[i]] + '</p>';
+                        '<p>' + curriculum.education.list[j].location[langs[i]] + '</p>' +
+                        '<p class="padding-left">' + curriculum.education.list[j].other[langs[i]] + '</p>';
         }
 
         codeHTML += '<br></div>';
@@ -58,7 +94,7 @@ function generateSlides(curriculum, langs) {
         $('#slidesContainer > .slide.education').append(codeHTML);   // end education info
 
 
-        codeHTML =  '<div class="' + langs[i] + ' content">' +
+        /*codeHTML =  '<div class="' + langs[i] + ' content">' +
                         '<h2>' + curriculum.courses.title[langs[i]] + '</h2>';
 
         for (let j=0; j<curriculum.courses.list.length; j++) {
@@ -70,22 +106,7 @@ function generateSlides(curriculum, langs) {
 
         codeHTML += '</div>';
 
-        $('#slidesContainer > .slide.courses').append(codeHTML);    // end courses info
-
-
-        codeHTML =  '<div class="' + langs[i] + ' content">' +
-                        '<h2>' + curriculum.workExp.title[langs[i]] + '</h2>';
-
-        for (let j=0; j<curriculum.workExp.list.length; j++) {
-            codeHTML += '<h3 class="bold nbsp">' + curriculum.workExp.list[j].title[langs[i]] + '</h3>' +
-                        '<h3 class="size01 nbsp lineBreak">' + curriculum.workExp.list[j].location[langs[i]] + '</h3>' +
-                        '<h3 class="size01 floatR lineBreak">' + curriculum.workExp.list[j].period[langs[i]] + '</h3>' +
-                        '<p>' + curriculum.workExp.list[j].description[langs[i]] + '</p>';
-        }
-
-        codeHTML += '</div>';
-
-        $('#slidesContainer > .slide.workExp').append(codeHTML);    // end work experience info
+        $('#slidesContainer > .slide.courses').append(codeHTML);    // end courses info*/
 
 
         codeHTML =  '<div class="' + langs[i] + ' content">' +
@@ -209,22 +230,31 @@ function recursiveSlideNext(distance) {
 }
 
 function organizeSlides() {
-    slideHeight = $(".section.slides .slide.current").height() + 40;
-    slideLeft = $(".section.slides").width() + 20;
+    if (!isMobile.any()) {
+        slideHeight = $(".section.slides .slide.current").height() + 40;
+        slideLeft = $(".section.slides").width() + 20;
 
-    $(".section.slides .slide").css({
-        display: "none",
-        position: "absolute",
-        left: slideLeft
-    });
+        $(".section.slides .slide").css({
+            display: "none",
+            position: "absolute",
+            left: slideLeft
+        });
 
-    $(".section.slides .slide.current").css({
-        display: "block",
-        left: 0
-    });
+        $(".section.slides .slide.current").css({
+            display: "block",
+            left: 0
+        });
 
-    $(".section.slides .slide.previous").css("left", -slideLeft);
-    $(".section.slides").css("height", slideHeight);
+        $(".section.slides .slide.previous").css("left", -slideLeft);
+        $(".section.slides").css("height", slideHeight);
+    } else {
+        $(".slide.workExp").css("padding", "20px 30px 8px 30px");
+        $(".slide.education.courses").css("padding", "8px 30px 8px 30px");
+        $(".slide.langs").css("padding", "8px 30px 20px 30px");
+        $(".previowsArrow").css("display", "none");
+        $(".nextArrow").css("display", "none");
+        $(".section.slideCounter").css("display", "none");
+    }
 }
 
 
