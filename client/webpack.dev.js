76fb614f54672811b62638dc71c6b617cc108a66
Chema <chema22r@gmail.com>
2019-09-18 22:45:32 +0200

    webpack config was modified (env vars)

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,16 +1,10 @@
 const common = require("./webpack.config.js");
 const merge = require("webpack-merge");
-const webpack = require("webpack");
 
 module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
     devServer: {
         contentBase: "./dist"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            DATA_URL: JSON.stringify(process.env.DATA_URL || "https://api.github.com/gists/09a7621fddbd018bc830fb3f5cd1fadb")
-        })
-    ]
+    }
 });
