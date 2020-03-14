1be4cac2980b6ebc410bce6401324ffd983622eb
Jose Maria Rivera <chema22r@gmail.com>
2020-03-14 01:10:53 -0700

    073 improve view (#74)
    * particles animation was added and CV button was removed
    
    * bug in safari with particles animation
    
    * bug in browsers with cards animations
    
    * bad indentation removed
    
    * particles animation modified
    
    * adjusted the width to solve a bug with small displays

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
+import "particles.js";
+
 import "./assets/style/normalize.css";
 import "./assets/style/index.css";
-import "./assets/style/navbar.css";
 import "./assets/style/projects.css";
 import "./assets/style/curriculum.css";
 
+import "./app/particles";
 import "./app/general";
 import "./app/projects";
 import "./app/curriculum";
