56e6daa721c90134b00c2b21e61b912bb1eea9fa
Jose Maria Rivera <chema22r@gmail.com>
2021-02-20 00:28:21 +0100

    added external NPM personal page to CV

--- client/src/app/curriculum.js
+++ client/src/app/curriculum.js
@@ -3,6 +3,7 @@
 generateSlides({
     "linkedin": "https://www.linkedin.com/in/chema22r",
     "github": "https://github.com/chema22r",
+    "npm": "https://www.npmjs.com/settings/chema22r/packages",
     "codepen": "https://codepen.io/chema22r",
     "driveCv": "https://drive.google.com/drive/folders/14_t2L92eh98rlP80SueieMA6bYGxA4pA",
     "personal": {
@@ -112,6 +113,7 @@ function generateSlides(curriculum) {
                 '</div>' +
                 '<a class="content icon-lkdn icon" href="' + curriculum.linkedin + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-gthb icon" href="' + curriculum.github + '" target="_blank" rel="external"></a>' +
+                '<a class="content icon-npm icon" href="' + curriculum.npm + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-cdpn icon" href="' + curriculum.codepen + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-gml icon" href="mailto:' + curriculum.personal.email + '" rel="external"></a>' +
                 '<a class="content icon-cv icon" href="' + curriculum.driveCv + '" target="_blank" rel="external"></a>';
@@ -179,6 +181,7 @@ function generateSlides(curriculum) {
 
     codeHTML =  '<a class="content icon-lkdn icon" href="' + curriculum.linkedin + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-gthb icon" href="' + curriculum.github + '" target="_blank" rel="external"></a>' +
+                '<a class="content icon-npm icon" href="' + curriculum.npm + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-cdpn icon" href="' + curriculum.codepen + '" target="_blank" rel="external"></a>' +
                 '<a class="content icon-gml icon" href="mailto:' + curriculum.personal.email + '" rel="external"></a>' +
                 '<a class="content icon-cv icon" href="' + curriculum.driveCv + '" target="_blank" rel="external"></a>';
