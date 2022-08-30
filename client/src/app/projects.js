8f5defb6416462be97c6f4f675d7e0f37c6e2978
Jose Maria Rivera <chema22r@gmail.com>
2022-08-30 02:16:43 +0200

    replaced health urls

--- client/src/app/projects.js
+++ client/src/app/projects.js
@@ -14,7 +14,7 @@ addProjects([
         "title": "Printers Discovery",
         "description": "Application with the aim of analyze the local network in search of prototypes of printers, capturing and storing their information.<br>Through the interface, users can modify the information of the detected printers and reserve them to work freely for a period of time.",
         "url": "https://printers-discovery.chema22r.com",
-        "statusCheckUrl": "https://printers-discovery.herokuapp.com/general/checkStatus",
+        "statusCheckUrl": "https://printers-discovery.onrender.com/general/checkStatus",
         "github": "https://github.com/Chema22R/printers-discovery"
     },
     {
@@ -22,7 +22,7 @@ addProjects([
         "title": "3D Previewer",
         "description": "3D objects previewer that allows users to interact and carry out certain basic operations on loaded objects, enabling their analysis in detail.<br>In addition, the application stores the extracted and processed geometry of the objects, so that they can be easily loaded again.",
         "url": "https://3d-previewer.chema22r.com",
-        "statusCheckUrl": "https://previewer-3d.herokuapp.com/checkStatus",
+        "statusCheckUrl": "https://previewer-3d.onrender.com/checkStatus",
         "github": "https://github.com/Chema22R/3d-previewer"
     },
     {
@@ -30,7 +30,7 @@ addProjects([
         "title": "Memoriizu",
         "description": "Application focused on language learning, in which users add the content they want to study, in addition to the period in which they want the content to be distributed.<br>A session is generated daily with the content that users should complete for that day, emphasizing the failures committed in previous sessions.",
         "url": "https://memoriizu.chema22r.com",
-        "statusCheckUrl": "https://memoriizu.herokuapp.com/checkStatus",
+        "statusCheckUrl": "https://memoriizu.onrender.com/checkStatus",
         "github": "https://github.com/Chema22R/memoriizu"
     }
 ]);
