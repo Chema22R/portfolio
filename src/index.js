90d0c0d406e5a706f4f3891a1f345a05ae04c90d
Jose Maria Rivera <chema22r@gmail.com>
2023-03-18 21:59:01 +0100

    style: update style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import "particles.js";
+import 'particles.js';
 
-import "./assets/style/normalize.css";
-import "./assets/style/index.css";
-import "./assets/style/projects.css";
-import "./assets/style/curriculum.css";
+import './assets/style/normalize.css';
+import './assets/style/index.css';
+import './assets/style/projects.css';
+import './assets/style/social.css';
 
-import "./app/particles";
-import "./app/general";
-import "./app/projects";
-import "./app/curriculum";
+import './app/particles';
+import './app/general';
+import './app/projects';
 
-document.body.style.opacity = "1";
+document.body.style.opacity = '1';
